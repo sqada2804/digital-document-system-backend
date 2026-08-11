abf4f9e2f671e7a90d88b6b4ba8426da8f910e03
sqada2804 <sqada2804@gmail.com>
2026-08-10 23:16:11 -0600

    setting the environment to notificaction-microservice

---
+++ notification-microservice/notification-microservice/src/test/java/com/example/notification_microservice/NotificationMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notification_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
